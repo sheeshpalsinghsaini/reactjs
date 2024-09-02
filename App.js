0c5883e772cae36a19a7db6e1a3a8dcc51fd9cb3
Sheeshpal Singh <123sheeshpalsinghsaini@gmail.com>
2024-09-02 23:51:25 +0530

    learn about babel, functional component.

--- App.js
+++ App.js
@@ -1,41 +1,71 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React element
+
+const heading = React.createElement("h1", { id: "heading" }, "React Course🚀"); // this is an js object
+
+// when we render it in dom then convert into html
+
+// JSX: provide easy syntax for react, not necessary to build react applicaton.
+// jsx is not html in javascript, it's html like syntax or xml like syntax.
+// jsx => React.createElement => ReactElement-JS Ojbect => HTMLElement(render).
+// const jsxHeading = <h1 id="heading" className="first-heading">React course using JSX 🚀</h1>
+
+// React element
+const jsxHeading = (
+  <h1 id="heading" className="first-heading">
+    React course using JSX 🚀
+  </h1>
+);
+
+console.log(heading);
+console.log(jsxHeading);
 
 /*
-    create a nested structure
-
-    <div>
-        <div id="parent">
-            <h1></h1>    
-        </div>
-    </div>
+    React Component 
+      1. Class Based Component : OLD
+      2. Functional Component : NEW
 
 
 
 
 */
 
-const parent = React.createElement("div",{id:"parent"},
-    [React.createElement("div",{id:"child",},
-        [ React.createElement("h1",{},"This is a React course🚀"),
-            React.createElement("h2",{},"I'm h2 tage")
-         ]
-    ),
-    React.createElement("div",{id:"child",},
-        [ React.createElement("h1",{},"I'm a h1 tag"),
-            React.createElement("h2",{},"I'm h2 tage")
-         ]
-    )]
-);
+// react function component : return React element
 
-const heading = React.createElement("h1",
-     {id:"heading",xyz:"xyz"}, // we can provide class and id here related to tag.
-     "Hello World from React!");
+// const HeadingComponent = () => {
+//     return <h1>React course Functional Component</h1>
+// }
 
-console.log(heading);
-console.log(parent);
+// const HeadingComponent = () => <h1 className="heading">React course Functional Component</h1>
+
+// const Title = () => (
+//     <h1 className="title">React course using JSX 🚀</h1>
+// )
+
+const Title = function () {
+  return <h1 className="title">React course using JSX 🚀</h1>;
+};
+
+const HeadingComponent = () => (
+  <div id="container">
+    {/* to run javascript inside jsx, we can run inside {} bracket 
+        jsx after sanatization inject data, it take care of mellecious data don't inject.
+    */}
+    {heading}
+    {console.log("kjasdfhkajdfhlasdjf;laskdfjslkjdaflkdjf")}
+    {/* inject Title Component inside Heading Component. this is called component composition */}
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h1 className="heading">React course Functional Component</h1>
+    <h1 className="heading">React course Functional Component</h1>
+    <h1 className="heading">React course Functional Component</h1>
+  </div>
+); // multiline functional component.
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading); // responsible to put heading into root element.
 
-root.render(parent);
+// root.render(jsxHeading); // will replace with root element from html
+root.render(<HeadingComponent />); // render functional component.
--- Notes.js
+++ Notes.js
@@ -1,60 +1,21 @@
 /*
-    package.json: is the npm configuration
+   we are using npx parcel index.html to run the server.
 
-    - whole code need to bunder before send to production, bundler help this.
-    - wheat and parcel are provide bundling features.
-    npm install -D parcel
-
-    dev dependencies: required when develope app.
-    normal dependencies: normal in development and in production
-
-
-    -D : representae development dependencies, not production dependencies.
-    ^ : (caret), miner version upgrade like 2.0.1 to 2.0.2 - 2.0.x
-    ~ : (tilde), major upgrade like 2.0.1 to 3.0.1 
-
-    npm install -D parcel : without -D it would be normal dependencies.
-
-
-    package-lock.json: keep the track of exact version.[lock ther version and keep the track of the version]
-
-    node_modules: fetch from npm init at the time of initialization
-                  - contains actual package of dependencies we configured.
-                  - each dependency is depend on another dependency that is called transitive dependencies.
-                  
-
-    - if we have package-lock.json, no need to put node_modules to put into github, when we will hit 
-      npm init, it will install all node_modules.
-
-    - npx parcel index.htm : parcel host our index.html on localhost:1234 server.
-    - npx: executing packages. 
-    
-    
-    - HMR[Hote module Replacement], read all file and have trace of everything parcel do to refresh the server live,
-    - parcel use file watching algorithm - written in c++
-
-    - parcel use caching, for faster build, when run again and again, it take lesser time.
-    - parcel to image optimization
-    - bundle all files 
-    - minification
-    - compress all files
-    - consistent hashing
-    - code splitting
-    - differential bundling 
-    - tree shaking: remove unused code.
-    - different dev and prod bundles
-
-npx parcel build index.html: generate production readyk app
-
-    - index.html, index.fed.css index.e4.js in dist folder are main file.
-
-    - that file can regenerate we should ignore them to put into server.
+   - we can change the script for this from package.json in 
+    "scripts": {
+                "start": "parcel index.html",
+                "build":"parcel build index.html",
+                "test": "jest"
+            }
+    now we need to run just `npm run start` or npm start work same.
+    npm build start // ready for production.
 
 
 
 
-
-
+const jsxHeading = <h1>React course using JSX 🚀</h1> 
+    - this is not understandable for javascript engine, parsel is working behind this.
+    - it is transpiled before going to js engine by parcel using Babel .
 
 
 
